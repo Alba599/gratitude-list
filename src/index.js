f313708b5a7176d27f3145746676e63617d16b90
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-05-11 08:01:28 -0500

    export form

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import FormPage from "./FormPage";
+import Form from "./Form";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
