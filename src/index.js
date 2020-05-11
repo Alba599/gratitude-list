4b986ad2abd4ba79509953a48bbfba362bd45930
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-05-11 09:30:46 -0500

    back to App

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Form from "./Form";
+// import Form from "./Form";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Form />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
