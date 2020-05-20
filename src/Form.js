7064ceafd1d2eb711341c17642a443e8462cc232
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-05-20 07:53:27 -0500

    form update

--- src/Form.js
+++ src/Form.js
@@ -30,27 +30,7 @@ const Form = () => {
         />
       </div>
       <div class="form-group">
-        <label for="exampleSelect1">Example select</label>
-        <select class="form-control" id="exampleSelect1">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="exampleSelect2">Example multiple select</label>
-        <select multiple class="form-control" id="exampleSelect2">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="exampleTextarea">Example textarea</label>
+        <label for="exampleTextarea">Enter text here</label>
         <textarea class="form-control" id="exampleTextarea" rows="3" />
       </div>
 
